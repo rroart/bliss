4081098727e816df27370c267b5f402352f2b3f3
Roar Thronæs <roart@nvg.ntnu.no>
2002-09-07 08:13:02 +0000

    new

---
+++ gcc/bliss/t.c
@@ -0,0 +1,20 @@
+int amalloc(){
+return malloc(4);
+}
+undecl(int x, int y){
+printf("undecl %x %x\n",x,y);
+}
+undecl2(void){
+printf("undecl2\n");
+}
+int d;
+main(){
+int c;
+int ee_=3;
+int *ee=&ee_;
+printf("ee %x %x %x\n",ee_,ee,*ee);
+printf("%x %x\n",&c, &d);
+s();
+t();
+dummy();
+}
