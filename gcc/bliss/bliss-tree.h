c140c107d7aa493cc865cf05ff6079b5a6544d47
Roar Thronæs <roart@nvg.ntnu.no>
2003-02-24 16:36:49 +0000

    our own tree codes

--- gcc/bliss/bliss-tree.h
+++ gcc/bliss/bliss-tree.h
@@ -1,6 +1,7 @@
 #define DEFTREECODE(SYM, NAME, TYPE, LENGTH) SYM,
 enum bliss_tree_code {
-  __DUMMY = LAST_AND_UNUSED_TREE_CODE,
+//  __DUMMY = LAST_AND_UNUSED_TREE_CODE,
+    __BLISSDUMMY = LAST_C_TREE_CODE,
 #include "bliss-tree.def"
   LAST_BLISS_TREE_CODE
 };
